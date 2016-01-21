a400907452250389a26516d3247216da0384088d
Christian Meier <meier.kristian@gmail.com>
2016-01-21 14:31:27 +0100

    new versions

--- jruby9-common/src/main/java/org/torquebox/mojo/jruby9/Versions.java
+++ jruby9-common/src/main/java/org/torquebox/mojo/jruby9/Versions.java
@@ -3,8 +3,8 @@ package org.torquebox.mojo.jruby9;
 public final class Versions {
     private Versions(){}
     
-    public final static String JRUBY = "9.0.0.0";
-    public final static String JRUBY_MAINS = "0.4.0";
+    public final static String JRUBY = "9.0.4.0";
+    public final static String JRUBY_MAINS = "0.5.0";
     public final static String JRUBY_RACK = "1.1.18";
-    public final static String JETTY = "8.1.14.v20131031";
+    public final static String JETTY = "8.1.16.v20140903";
 }
